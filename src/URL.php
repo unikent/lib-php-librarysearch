bee1228f95639ac679b0b02e6c4087e519c52f93
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-11-17 13:11:40 +0000

    Switch namespace

--- src/URL.php
+++ src/URL.php
@@ -7,7 +7,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-namespace UKC\LibrarySearch;
+namespace unikent\LibrarySearch;
 
 /**
  * Represents a Library Search URL.
