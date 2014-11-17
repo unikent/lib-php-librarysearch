323bf26d71d08eec1c3755a11a3c4c61dffa58ee
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-11-17 13:28:23 +0000

    Implement get_search_url

--- src/URL.php
+++ src/URL.php
@@ -49,4 +49,54 @@ class URL
     public function set_search_term($searchterm) {
         $this->_search_term = $searchterm;
     }
+
+    /**
+     * Returns the base URL.
+     * 
+     * @see get_url()
+     */
+    public function get_base_url() {
+        return 'http://primo-direct-eu-sb.hosted.exlibrisgroup.com/primo_library/libweb/action/search.do';
+    }
+
+    /**
+     * Returns the URL params.
+     */
+    public function get_url_params() {
+        return array(
+            'fn' => 'search',
+            'ct' => 'search',
+            'initialSearch' => true,
+            'mode' => 'Basic',
+            'tab' => 'default_tab',
+            'indx' => 1,
+            'dum' => true,
+            'srt' => 'rank',
+            'vid' => '44KEN_VU1',
+            'frbg' => '',
+            'vl%28freeText0%29' => urlencode($this->_search_term)
+        );
+    }
+
+    /**
+     * Returns a URL to library search.
+     */
+    public function get_search_url() {
+        $baseurl = $this->get_base_url();
+
+        $params = $this->get_url_params();
+        $params = array_map(function($param, $value) {
+            return "{$param}={$value}";
+        }, $params);
+        $params = implode('&', $params);
+
+        return "{$baseurl}?{$params}";
+    }
+
+    /**
+     * To String!
+     */
+    public function __toString() {
+        return $this->get_url();
+    }
 }
