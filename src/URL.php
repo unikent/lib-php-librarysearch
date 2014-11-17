33bc9555f4f05209661211e9f119852397399c06
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-11-17 13:08:14 +0000

    Implement set_campus

---
+++ src/URL.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Reading Lists API for is-dev applications.
+ *
+ * @package    ReadingLists
+ * @copyright  2014 Skylar Kelty <S.Kelty@kent.ac.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace UKC\LibrarySearch;
+
+/**
+ * Represents a Library Search URL.
+ */
+class URL
+{
+    /** Campus */
+    private $_campus;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        $this->set_campus('canterbury');
+    }
+
+    /**
+     * Set the campus of the URL.
+     */
+    public function set_campus($campus) {
+        if (!in_array($campus, array('canterbury', 'medway'))) {
+            throw new \Exception("Invalid campus '{$campus}'.");
+        }
+
+        $this->_campus = $campus;
+    }
+}
