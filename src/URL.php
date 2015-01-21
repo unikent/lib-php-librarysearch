26618b8bfcdc132e8be441ba1cc01c5474d51e5b
Jake <J.Blatchford@kent.ac.uk>
2015-01-21 11:42:10 +0000

    Remove extra /

--- src/URL.php
+++ src/URL.php
@@ -116,7 +116,7 @@ class URL
      * @see get_url()
      */
     public function get_base_url() {
-        return 'http://pmt-eu.hosted.exlibrisgroup.com//primo_library/libweb/action/search.do';
+        return 'http://pmt-eu.hosted.exlibrisgroup.com/primo_library/libweb/action/search.do';
     }
 
     /**
